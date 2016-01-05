20bcdd20d74778d3a8ec9442c56936061967ae38
demoleas21 <andrew.demoleas@gmail.com>
2016-01-05 15:12:48 -0500

    disabled urls.py & wsgi.py for testing

--- andrewsite/andrewsite/urls.py
+++ andrewsite/andrewsite/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
+
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+]"""
