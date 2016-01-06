a95cd9dad0e0acb3a9d8cfef5ed8c4dc632f4fcc
demoleas21 <andrew.demoleas@gmail.com>
2016-01-05 20:29:51 -0500

    added pylint disable functions

--- andrewsite/andrewsite/wsgi.py
+++ andrewsite/andrewsite/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-
+"""
 
 import os
 
@@ -13,5 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "andrewsite.settings")
 
-application = get_wsgi_application()
-"""
+application = get_wsgi_application() #pylint:  disable=invalid-name
